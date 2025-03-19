d9bac495c67d1a9ab7a64f9b58a7521ab60e0dfe
Prasad D <prasaddarne3@gmail.com>
2025-03-19 15:28:39 +0530

    Added appspec.yml and start script

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+<<<<<<< HEAD
 const port = 3000;
 
 app.get('/', (req, res) => {
@@ -8,4 +9,14 @@ app.get('/', (req, res) => {
 
 app.listen(port, '0.0.0.0', () => {
   console.log(`Server running at http://localhost:${port}`);
+=======
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, Dockerized Node.js App!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+>>>>>>> f30a208 (Initial commit)
 });
---
+++ start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/ec2-user/my-node-app
+npm install
+node index.js &
